0901022605b43650766ed7e1b4357ef8d4fa9a84
Andrés Arana <and2arana@gmail.com>
2016-09-21 22:33:07 +0200

    Fixes multirule parsing on reloadable environments
    When the environment reloads, we were reusing the same parser which
    caused rules to start accumulating each time the loader re-parses an
    element.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var nearley = require('nearley/lib/nearley.js');
 var compile = require('nearley/lib/compile.js');
 var generate = require('nearley/lib/generate.js');
-
 var grammar = require('nearley/lib/nearley-language-bootstrapped.js');
-var parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
 
 module.exports = function(input) {
+  var parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
   parser.feed(input);
 
   var compilation = compile(parser.results[0], {});
